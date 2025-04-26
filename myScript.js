bba2915f806a482b61fcb4002e36ea91b4f4de0c
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-04-26 11:12:23 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,7 +2,3 @@
   function fun1() {
     document.querySelector('.cssGridContainer1').classList.toggle("-toggled");
   }
-function fun2(){
-          this.whichDb = await getEnvVars.getWhichDb() //duznt work?
-
-}
